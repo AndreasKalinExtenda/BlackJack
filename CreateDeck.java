b06b49503078cd9142b7162b9ed4978d3e290aa7
Andreas Kalin <andreas.kalin@extendaretail.com>
2023-01-08 00:34:08 +0100

    Added the Deck() constructor

--- CreateDeck.java
+++ CreateDeck.java
@@ -1,10 +1,20 @@
+import java.util.Random;
 public class CreateDeck {
-    Suit suit;
-    Denomination denomination;
+    //Suit suit;
+    //Denomination denomination;
 
-    public Cards (Suit suit, Denomination denomination) {
-        this.suit = suit;
-        this.denomination = denomination;
+    //public Cards (Suit suit, Denomination denomination) {
+    //    this.suit = suit;
+    //    this.denomination = denomination;
+    Card[] cards;
+
+    public deck() {
+        cards = new Card[52];
+        int index = 0;
+        for (Suit suit : Suit.values()) {
+            for (Denomination denomination : Denomination.values()) {
+                cards[index] = new Card (denomination, suit);
+            }
+        }
     }
-
 }
