398068ee950fac936c2d9d2e773207ba73914b1a
Andreas Kalin <andreas.kalin@extendaretail.com>
2023-01-07 23:32:41 +0100

    Initial commit for BlackJack game. Java project and Deck class

---
+++ Deck.java
@@ -0,0 +1,12 @@
+public class Deck {
+    String suit;
+    Int denomination;
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public Int getDenomination() {
+        return denomination;
+    }
+}
